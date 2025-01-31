0543ee0697301f01320fc66ed13f0da0f1a78e47
Vinayak Tiwari <159702509+vintiw6@users.noreply.github.com>
2025-01-31 13:12:51 +0530

    updated it

---
+++ Assignment 1/Conditional Vector Transformation.R
@@ -0,0 +1,7 @@
+# 25. Convert the vector c(3,1,2,3,1,2,3,1,2) into a vector of only 2s, using a vector of length 2. Which vectorized operations in R would facilitate this conversion?
+
+Vector <- c(3, 1, 2, 3, 1, 2, 3, 1, 2)
+
+# Convert the vector to only 2s using a vector of length 2
+convector <- rep(2, length(Vector))
+convector
