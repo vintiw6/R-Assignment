f536c049f23c2fff74af48cef2ae0df8a72675bf
Vinayak Tiwari <159702509+vintiw6@users.noreply.github.com>
2025-02-05 22:56:21 +0530

    moved file

--- Assignment 2/q11.R
+++
@@ -1,8 +0,0 @@
-# 11. Extract and store as a new object the fourth- and first-row elements, in that order, of the second column only of all layers of (10)
-
-sequence <- seq(4.8, 0.1, length.out = 48)
-arr <- array(sequence, dim = c(4, 2 , 6))
-print(arr)
-
-extracted_values <- arr[c(4, 1), 2, ]
-print(extracted_values)
--- Assignment 2/q17.R
+++
@@ -1,9 +0,0 @@
-# 17. Sort the first column of the matrix from question (15.) in descending order. Keep other columns unchanged.
-
-mat <- matrix(1:15, nrow = 5, ncol = 3, byrow = FALSE)
-
-
-mat_sorted <- mat
-mat_sorted[, 1] <- sort(mat[, 1], decreasing = TRUE)
-
-print(mat_sorted)
